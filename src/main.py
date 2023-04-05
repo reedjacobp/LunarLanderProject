0d2e117ed8fa7c233355042196f1f99ffff73420
reedjacobp <reed.jacobp@gmail.com>
2023-04-04 21:32:31 -0600

    discretize states and update policy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-from gym_envs.lunar_lander import LunarLander
+# from gym_envs.lunar_lander import LunarLander
+import gymnasium as gym
+import numpy as np
+from algorithms.q_learning import QLearning
 
 def main():
-    env = LunarLander()
+    env = gym.make("LunarLander-v2", render_mode="human")
+    # env.seed(0)
+    # np.random.seed(0)
+    
     # Run your training or testing loop here
+    q = QLearning(env)
+    q.run_episode(env, n_episodes=1_200_000)
 
 if __name__ == '__main__':
     main()
