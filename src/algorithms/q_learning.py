450f5cda7b0ce457dc971222fcbe3e3aae87b1db
Jacob Reed <48200417+reedjacobp@users.noreply.github.com>
2023-04-16 17:13:31 -0600

    Final updates for Q learning (#5)
    * adding links to README
    
    * add initial set of files for environment
    
    * add skeleton classes
    
    * discretize states and update policy
    
    * adding stuff for plotting
    
    * Update README.md
    
    * final updates for Q learning

--- src/algorithms/q_learning.py
+++ src/algorithms/q_learning.py
@@ -1,19 +1,38 @@
-from utils.policy import EpsilonGreedyPolicy
+import os
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
 import numpy as np
+import copy
+import itertools
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from utils.policy import EpsilonGreedyPolicy
+from gymnasium.utils.save_video import save_video
 
 class QLearning:
-    def __init__(self, env, gamma=0.99, learning_rate=0.01, epsilon=0.10):
+    def __init__(self, env, n_episodes=1000, gamma=0.99, learning_rate=0.0001, epsilon=0.1):
+        self.env = env
+        self.n_episodes = n_episodes
         self.gamma = gamma
         self.learning_rate = learning_rate
         self.epsilon = epsilon
-        self.policy = EpsilonGreedyPolicy(epsilon)
+        self.policy = EpsilonGreedyPolicy()
         self.state_dict = {}
-        self.Q = None
+        self.step_starting_index = 0
+        self.video_path = os.path.abspath(os.path.join(this_dir, '..', '..', 'data', 'qlearning', 'videos'))
+        self.plots_path = os.path.abspath(os.path.join(this_dir, '..', '..', 'data', 'qlearning', 'plots'))
+        self.qlearning_data_path = os.path.abspath(os.path.join(this_dir, '..', '..', 'data', 'qlearning'))
 
         # Initialize state dictionary
+        # For the first 6 states, it will have 12 linearly spaced discretized points (num=12) 
+        # and the last 2 states will have 2 linearly spaced discretized points
         sample_obs = env.observation_space.sample()
         for i, _ in enumerate(sample_obs):
-            self.state_dict[i] = np.linspace(env.observation_space.low[i], env.observation_space.high[i], num=env.observation_space.shape[0])
+            if i < 6:
+                self.state_dict[i] = np.linspace(env.observation_space.low[i], env.observation_space.high[i], num=12)
+            elif i >= 6:
+                self.state_dict[i] = np.linspace(env.observation_space.low[i], env.observation_space.high[i], num=2)
 
     def discretize_state(self, state):
         """Discretize continuous state to nearest discrete state."""
@@ -30,41 +49,146 @@ class QLearning:
                 disc_state.append(0)
         return tuple(disc_state)
 
-    def run_episode(self, env, n_episodes):
-        self.Q = np.zeros((env.observation_space.shape[0], env.action_space.n))
+    def create_q(self):
+        """Helper function to create Q table"""
+        print("Creating Q table...")
+        discrete_sizes = [self.state_dict[s].size for s in range(self.env.observation_space.shape[0])]
+        Q = {}
+        for state_tuple in itertools.product(*[range(size) for size in discrete_sizes]):
+            s = state_tuple
+            for a in range(self.env.action_space.n):
+                Q[(s,a)] = 0.0
+        print("Finished creating Q table!")
+        return Q
+
+    def run_episode(self):
+
+        Q = self.create_q()
         episodes = []
-
-        for _ in range(n_episodes):
-            obs = env.reset()
-            s = self.discretize_state(obs[0])
-            episodes.append(self.episode(s, env))
-
-        return episodes
+        with tqdm(total=self.n_episodes, desc='Running episodes', unit='episode') as pbar:
+            for ep_index in range(self.n_episodes):
+                episodes.append(self.episode(Q, self.env, ep_index, epsilon=max(self.epsilon, 1-ep_index/self.n_episodes)))
+                pbar.update(1)
+            return episodes
 
 
-    def episode(self, s, env):
-        terminated = False
-        truncated = False
+    def episode(self, Q, env, ep_index, epsilon, max_steps=500):
+
+        # First pass
+        obs = env.reset()
+        s = self.discretize_state(obs[0])
+        a = self.policy.select_action(s, list(range(env.action_space.n)), Q, epsilon)
+        obs, r, terminated, truncated, info = env.step(a)
+        sp = self.discretize_state(obs)
         hist = [s]
+        i = 0
 
-        for _ in range(1000):
-            # Choose action
-            a = self.policy.select_action(s, list(range(env.action_space.n)), self.Q)
+        while not terminated and not truncated and i < max_steps:
 
-            # Take action
+            # Temporal Difference Update
+            q_hat = r + self.gamma * np.max([Q[(sp, ap)] for ap in list(range(env.action_space.n))])
+
+            # Learning Rate * Temporal Difference
+            Q[(s, a)] += self.learning_rate * (q_hat - Q[(s, a)])
+
+            # Stuff for next iteration
+            s = sp
+            a = self.policy.select_action(s, list(range(env.action_space.n)), Q, epsilon)
             obs, r, terminated, truncated, info = env.step(a)
             sp = self.discretize_state(obs)
+            hist.append(s)
+            i += 1
 
             if terminated or truncated:
+                save_video(
+                    env.render(),
+                    video_folder=self.video_path,
+                    fps=env.metadata["render_fps"],
+                    name_prefix="qlearning-video",
+                    step_starting_index=self.step_starting_index,
+                    episode_index=ep_index
+                )
+                self.step_starting_index += i
+
+        return hist, Q
+    
+    def evaluate(self, env, policy, n_episodes_eval=10000, max_steps=500):
+        
+        # Limit the amount of episodes for evaluation
+        if self.n_episodes > 10000:
+            n_episodes_eval = 10000
+        else:
+            n_episodes_eval = self.n_episodes
+
+        with tqdm(total=n_episodes_eval, desc='Evaluating', unit='episode') as pbar:
+            returns = []
+            for _ in range(n_episodes_eval):
+                t = 0
+                r = 0.0
                 obs = env.reset()
                 s = self.discretize_state(obs[0])
-            else:
-                # Update Q value
-                q_hat = r + self.gamma * np.max(self.Q[(sp, a)])
-                self.Q[(sp, a)] += self.learning_rate * (q_hat - self.Q[(sp, a)])
+                terminated = False
+                truncated = False
+                while not terminated and not truncated and t < max_steps:
+                    a = policy(s)
+                    obs, r_act, terminated, truncated, info = env.step(a)
+                    r += r_act
+                    s = self.discretize_state(obs)
+                    t += 1
+                returns.append(r)
+                pbar.update()
+        return returns
+    
+    def learn(self):
+        episodes = self.run_episode()
 
-                # Update state and history
-                s = sp
-                hist.append(s)
+        n = round(self.n_episodes/10)
+        stop = self.n_episodes
+        p1 = plt.subplot()
+        p1.set_xlabel('steps in environment')
+        p1.set_ylabel('avg return')
 
-        return hist, self.Q
+        print("Starting Q-table evaluation...")
+        # Initialize Q, x-axis, and y-axis
+        Q = self.create_q()
+        xs = [0]
+        ys = [np.mean([self.evaluate(self.env, lambda s: np.argmax([Q[(s, a)] for a in list(range(self.env.action_space.n))]))])]
+
+        # Loop through each episode
+        best_Q = None
+        best_mean = float('-inf')
+        with tqdm(total=(min(stop, len(episodes)) - n), desc='Evaluating Q-table') as pbar:
+            for i in range(n, min(stop, len(episodes)), n):
+
+                # This calculates how many steps were taken in this particular episode
+                newsteps = sum(len(ep[0]) for ep in episodes[i-n:i])
+                xs.append(xs[-1] + newsteps)
+
+                # Grab the Q-table from this particular episode
+                Q = episodes[i-n][1]
+
+                # Compute the mean of undiscounted rewards from a simulation
+                mean_reward = np.mean([self.evaluate(self.env, lambda s: np.argmax([Q[(s, a)] for a in list(range(self.env.action_space.n))]))])
+                ys.append(mean_reward)
+
+                # Keep track of the Q-table with the largest mean
+                if mean_reward > best_mean:
+                    best_Q = copy.deepcopy(Q)
+                    best_mean = mean_reward
+
+                # Update the progress bar
+                pbar.update(n)
+
+        # Plot the learning curve
+        p1.plot(xs, ys, label='Q-Learning')
+        plt.savefig(self.plots_path + '/learning_curve.png')
+
+        # Save Q data
+        np.save(self.qlearning_data_path + '/best_Q.npy', best_Q)
+
+        # Print out information
+        print(f'The best mean achieved was: {best_mean}')
+        print(f'The best Q was saved to {self.qlearning_data_path}/best_Q.npy')
+        print(f'Plots were saved to: {self.plots_path}')
+        print(f'Videos were saved to: {self.video_path}')
+        
--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
-# from gym_envs.lunar_lander import LunarLander
 import gymnasium as gym
-import numpy as np
 from algorithms.q_learning import QLearning
 
 def main():
-    env = gym.make("LunarLander-v2", render_mode="human")
-    # env.seed(0)
-    # np.random.seed(0)
+    env = gym.make("LunarLander-v2", render_mode="rgb_array_list")
     
-    # Run your training or testing loop here
-    q = QLearning(env)
-    q.run_episode(env, n_episodes=1_200_000)
+    # Do Q Learning
+    q = QLearning(env, n_episodes=50001)
+    q.learn()
+
+    # Do Double Q Learning
+
+    # Do SARSA
+
+    # Do DQN
 
 if __name__ == '__main__':
     main()
