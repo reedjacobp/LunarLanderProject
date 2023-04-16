a287861afaa7431e66121ef8d53777f87bd98220
Jacob Patrick Reed <reed.jacobp@gmail.com>
2023-04-16 17:09:25 -0600

    final updates for Q learning

--- src/main.py
+++ src/main.py
@@ -1,35 +1,19 @@
-# from gym_envs.lunar_lander import LunarLander
 import gymnasium as gym
 from algorithms.q_learning import QLearning
-import numpy as np
-import matplotlib.pyplot as plt
 
 def main():
-    env = gym.make("LunarLander-v2")
-    # env.seed(0)
-    # np.random.seed(0)
+    env = gym.make("LunarLander-v2", render_mode="rgb_array_list")
     
-    # Run your training or testing loop here
-    q = QLearning(env)
-    episodes = q.run_episode(env, n_episodes=100)
+    # Do Q Learning
+    q = QLearning(env, n_episodes=50001)
+    q.learn()
 
-    n = 10
-    stop = 100
-    p1 = plt.subplot()
-    p1.set_xlabel('steps in environment')
-    p1.set_ylabel('avg return')
-    for eps in episodes:
-        Q = np.zeros((env.observation_space.shape[0], env.action_space.n))
-        xs = [0]
-        # calc_returns = q.evaluate(env, lambda s: np.argmax([Q[(s, a)] for a in list(range(env.action_space.n))]))
-        ys = [np.mean(q.evaluate(env, Q))]
-        for i in range(n, min(stop, len(eps)), n):
-            newsteps = sum(len(ep.hist) for ep in eps[i-n+1:i])
-            xs.append(xs[-1] + newsteps)
-            Q = eps[i].Q
-            ys.append(np.mean([q.evaluate(env, lambda s: np.argmax([Q[(s, a)] for a in list(range(env.action_space.n))]))]))
-        p1.plot(xs, ys, label='Q-Learning')
-    plt.savefig('steps_in_env.png')
+    # Do Double Q Learning
+
+    # Do SARSA
+
+    # Do DQN
+
 
 if __name__ == '__main__':
     main()
--- src/utils/policy.py
+++ src/utils/policy.py
@@ -1,19 +1,14 @@
-import random
 import numpy as np
 
 class EpsilonGreedyPolicy:
-    def __init__(self, epsilon):
-        self.epsilon = epsilon
+    # def __init__(self):
+    #     # Use this as needed
 
     # Select action index based on epsilon-greedy policy
-    def select_action(self, s, actions, Q):
-        if random.random() < self.epsilon:
-            return random.randint(0, len(actions) - 1)
+    def select_action(self, s, actions, Q, epsilon):
+        if np.random.rand() < epsilon:
+            # Be random with a probability of epsilon
+            return np.random.randint(0, len(actions) - 1)
         else:
-            # I was having issues with the following line
-            # return np.argmax([Q[(s,a)] for a in actions])
-            values = [Q[(s,a)] for a in actions]
-            max_value = np.max(values)
-            max_indices = np.where(values == max_value)[0]
-            a = np.random.choice(max_indices)
-            return a
+            # Be greedy with a probability of 1-epsilon
+            return np.argmax([Q[(s, a)] for a in actions])
