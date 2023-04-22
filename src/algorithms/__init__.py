af5719ceb76df66e1b12894dfbc40469079867f4
Jacob Reed <48200417+reedjacobp@users.noreply.github.com>
2023-04-22 03:17:38 -0500

    Integrated DQN code (#6)
    * adding links to README
    
    * add initial set of files for environment
    
    * add skeleton classes
    
    * discretize states and update policy
    
    * adding stuff for plotting
    
    * Update README.md
    
    * final updates for Q learning
    
    * integrate dqn code
    
    * added some quality-of-life improvements

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,11 +1,12 @@
 from .q_learning import QLearning
 from .double_q_learning import DoubleQLearning
 from .sarsa import SARSA
-from .dqn import DQN
+from .dqn import DQN, DQNAgent
 
 __all__ = [
     'QLearning',
     'DoubleQLearning',
     'SARSA',
     'DQN',
+    'DQNAgent',
 ]
--- src/main.py
+++ src/main.py
@@ -1,18 +1,32 @@
 import gymnasium as gym
 from algorithms.q_learning import QLearning
+from algorithms.dqn import DQNAgent
 
 def main():
     env = gym.make("LunarLander-v2", render_mode="rgb_array_list")
     
-    # Do Q Learning
-    q = QLearning(env, n_episodes=50001)
-    q.learn()
+    algorithm = input("Enter algorithm to use (q, dq, sarsa, dqn): ")
 
-    # Do Double Q Learning
+    if algorithm == 'q':
+        q = QLearning(env, n_episodes=10000)
+        q.learn()
+    # TODO: Add Double Q-Learning and SARSA?
+    # elif algorithm == 'dq':
+    #     dq = DoubleQLearning(env, n_episodes=10000)
+    #     dq.learn()
+    # elif algorithm == 'sarsa':
+    #     sarsa = SARSA(env, n_episodes=10000)
+    #     sarsa.learn()
+    elif algorithm == 'dqn':
+        dqn_agent = DQNAgent(env, n_episodes=500)
+        dqn_agent.train()
 
-    # Do SARSA
+        # TODO: Add some logic to determine if the user wants to run a MC
+        dqn_agent.run_mc(msims=10)
+    else:
+        print("Invalid algorithm selected!")
 
-    # Do DQN
+    env.close()
 
 if __name__ == '__main__':
     main()
