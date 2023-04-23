71fba087888b9699790c9fb21342cbd04c247a93
Jacob Reed <48200417+reedjacobp@users.noreply.github.com>
2023-04-23 14:38:39 -0500

    Added stuff for plotting loss and using tanh instead of relu (#7)
    * adding links to README
    
    * add initial set of files for environment
    
    * add skeleton classes
    
    * discretize states and update policy
    
    * adding stuff for plotting
    
    * Update README.md
    
    * final updates for Q learning
    
    * integrate dqn code
    
    * added some quality-of-life improvements
    
    * iteration 3 updates - tanh instead of relu

--- src/main.py
+++ src/main.py
@@ -10,19 +10,21 @@ def main():
     if algorithm == 'q':
         q = QLearning(env, n_episodes=10000)
         q.learn()
-    # TODO: Add Double Q-Learning and SARSA?
-    # elif algorithm == 'dq':
-    #     dq = DoubleQLearning(env, n_episodes=10000)
-    #     dq.learn()
-    # elif algorithm == 'sarsa':
-    #     sarsa = SARSA(env, n_episodes=10000)
-    #     sarsa.learn()
     elif algorithm == 'dqn':
-        dqn_agent = DQNAgent(env, n_episodes=500)
-        dqn_agent.train()
+        # Iteration 1 should be using the relu activation function
+        # dqn_agent_iter1 = DQNAgent(env, n_episodes=500, lr=1e-4)
+        # dqn_agent_iter1.train()
+
+        # Iteration 2 should be using the relu activation function
+        # dqn_agent_iter2 = DQNAgent(env, n_episodes=400, lr=1e-4, batch_size=1000, mmem_size=1000000, max_steps=500, eps_dec=1e-4)
+        # dqn_agent_iter2.train()
+
+        # Iteration 3 should be using the tanh activation function
+        dqn_agent_iter3 = DQNAgent(env, n_episodes=50, lr=1e-4, batch_size=5, mmem_size=1000000, max_steps=500, eps_dec=1e-4)
+        dqn_agent_iter3.train()
 
         # TODO: Add some logic to determine if the user wants to run a MC
-        dqn_agent.run_mc(msims=10)
+        # dqn_agent.run_mc(msims=10)
     else:
         print("Invalid algorithm selected!")
 
